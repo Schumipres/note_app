e8660df77cb4f26858d486b859b23e90d16a08c4
Schumi <mattis.litot@epitech.eu>
2024-03-25 20:43:24 +0100

    init + todo part using cubit

---
+++ lib/todo_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/cubit/todo_cubit.dart';
+import 'package:todo_app/models/todo_model.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: BlocBuilder<TodoCubit, List<Todo>>(
+        builder: (context, state) {
+          return ListView.builder(
+            itemCount: state.length,
+            itemBuilder: (context, index) {
+              final todo = state[index];
+              return ListTile(
+                title: Text(todo.name),
+                subtitle: Text(todo.createdTime.toString()),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add-todo');
+        },
+        tooltip: 'Add Todo',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
